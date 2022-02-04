eaa5aadeb0e9997312ec1dfcf537f3a1740e76a8
Ana Luiza <analuiza@iamsweepi.com>
2022-02-04 14:14:52 -0300

    using list view builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,66 +12,62 @@ class TransactionsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
-      child: IntrinsicHeight(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: userTransactions.map((transaction) {
-              return Card(
-                child: Row(
+      height: 500,
+      child: ListView.builder(
+        itemCount: userTransactions.length,
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.elliptical(10.0, 10.0),
+                    ),
+                    border: Border.all(
+                      color: Colors.teal,
+                      width: 2,
+                    ),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    '\$${userTransactions[index].amount}',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                        color: Colors.teal),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 50),
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.all(
-                          Radius.elliptical(10.0, 10.0),
-                        ),
-                        border: Border.all(
-                          color: Colors.teal,
-                          width: 2,
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 10),
-                      child: Text(
-                        '\$${transaction.amount}',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                            color: Colors.teal),
+                    Text(
+                      userTransactions[index].title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w500,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.black87,
-                            fontWeight: FontWeight.w500,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('dd/MM/yy').format(transaction.date),
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.black45,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ],
-                    )
+                    Text(
+                      DateFormat('dd/MM/yy')
+                          .format(userTransactions[index].date),
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.black45,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
                   ],
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
